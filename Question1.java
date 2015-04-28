9f2b3369d1bef20ed858b76a5cd8a7e0e1542992
AM021961 <charlie19922991@gmail.com>
2015-04-28 18:05:02 +0800

    C AM021961 王璁提交

--- Question1.java
+++ Question1.java
@@ -1,6 +1,7 @@
 ﻿public class Question1{
 	public statis void main(String[] args){
+		System.out.println(“A”);
+		System.out.println(“C”);
 		System.out.println("Test");
-		System.out.println("B");
 	}
 }
